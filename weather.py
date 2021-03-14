1ffa2acf66e024197960bb6494ee9d6876e57877
codyjamesbrooks <71521423+codyjamesbrooks@users.noreply.github.com>
2021-03-13 20:21:42 -0700

    Added write to crags db

--- weather.py
+++ weather.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+weather = Flask(__name__)
 
 # Configure DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crags.db'
-db = SQLAlchemy(app)
+weather.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crags.db'
+db = SQLAlchemy(weather)
+
+# Google Maps API Key
+path = "..\API Keys\ConniesAPIkey.txt"
+with open(path, 'r') as f: 
+    maps_APIkey = f.read()
 
 
 # DB Class Model 
@@ -17,22 +22,40 @@ class ClimbingArea(db.Model):
     city = db.Column(db.String(100))
     state = db.Column(db.String(50))
 
+    def __init__(self, area_dict):
+        self.name = area_dict['name']
+        self.city = area_dict['city']
+        self.state = area_dict['state']
+        self.longitude = area_dict['longitude']
+        self.latitude = area_dict['latitude']
+
     def __str__(self):
         return self.name
 
 
 # Home page Route. 
-@app.route('/', methods=['GET', 'POST'])
-def index(): 
-    crags = ClimbingArea.query.all()
-    return render_template('home.html', crags=crags)
+@weather.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('home.html')
 
 
-@app.route('/addarea/', methods=['GET', 'POST'])
+@weather.route('/addarea/', methods=['GET', 'POST'])
 def add_crag():
     if request.method == 'POST':
-        # Add new climbing area. 
-        pass
+        area_dict = {   
+                    'name' : request.form['name'],
+                    'city' : request.form['city'],
+                    'state' : request.form['state'],
+                    'longitude' : request.form['longitude'],
+                    'latitude' : request.form['latitude']
+        }
+        add_area = ClimbingArea(area_dict)
+        try: 
+            db.session.add(add_area)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding the area"
     else: 
         # Display Form for adding a new crag
         return render_template('addarea.html')
@@ -41,6 +64,6 @@ def add_crag():
 
 # Call the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    weather.run(debug=True)
 
 
